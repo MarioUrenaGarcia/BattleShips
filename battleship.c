446534ecd5d352df331a8f680c5c51211d32ea0a
Mario Ureña García <a2318783@correo.uia.mx>
2023-11-19 18:00:26 -0600

    Creación de la Matriz del Tablero y Menú Principal

--- battleship.c
+++ battleship.c
@@ -15,7 +15,6 @@
 
 */
 
-
 // Declarar Estructuras ------------------------------------------------------------
 
 // Declarar funciones --------------------------------------------------------------
@@ -24,15 +23,15 @@ void main_menu();
 
 int main(int argc, char *argv[])
 {
-    printf("\n\n\tEste código fue creado por Mario Ureña García y Ricardo Ponce de León Vargas\n\n");
+  printf("\n\n\tEste código fue creado por Mario Ureña García y Ricardo Ponce de León Vargas\n\n");
 
-    // Variables
-
-    // Procesos
-    main_menu();
-    // FINALIZACIÓN DEL PROGRAMA
-    printf("\n\n\tPrograma Finalizado con ÉXITO\n\n");
-    return 0;
+  // Variables
+  int tablero[10][10];
+  // Procesos
+  main_menu();
+  // FINALIZACIÓN DEL PROGRAMA
+  printf("\n\n\tPrograma Finalizado con ÉXITO\n\n");
+  return 0;
 }
 
 // Funciones -----------------------------------------------------------------------
@@ -46,7 +45,7 @@ int main(int argc, char *argv[])
 void main_menu()
 {
   int input;
-  
+
   printf("\n1. Jugar contra Computadora");
   printf("\n2. Jugar contra Jugador");
   printf("\n3. Jugar contra Cargar Partida");
@@ -55,31 +54,31 @@ void main_menu()
 
   scanf("%d", &input);
 
-  switch(input)
-    {
-    case 1:
-      {
-	system("clear");
-	printf("Jugar contra Computadora");
-	break;
-      }
-    case 2:
-      {
-	system("clear");
-	printf("Jugar contra Jugador");
-	break;
-      }
-    case 3:
-      {
-	system("clear");
-	printf("Cargar Partida");
-	break;
-      }
-    case 4:
-      {
-	system("clear");
-	exit(0);
-	break;
-      }
-    }
+  switch (input)
+  {
+  case 1:
+  {
+    system("clear");
+    printf("Jugar contra Computadora");
+    break;
+  }
+  case 2:
+  {
+    system("clear");
+    printf("Jugar contra Jugador");
+    break;
+  }
+  case 3:
+  {
+    system("clear");
+    printf("Cargar Partida");
+    break;
+  }
+  case 4:
+  {
+    system("clear");
+    exit(0);
+    break;
+  }
+  }
 }
