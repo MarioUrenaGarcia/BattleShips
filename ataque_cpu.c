2440502bd810678a5a406ecb1d4f89c88d50d8b3
Ponce <poncerichi4@gmail.com>
2023-12-06 15:34:04 -0600

    creacion de rama para corregir errores en ataque

---
+++ ataque_cpu.c
@@ -0,0 +1,100 @@
+/*
+
+POR CORREGIR: QUE EL ATAQUE DE LA CPU SEA MAS EFICIENTE E INTELIGENTE
+
+El puntero de dirección (*direccion_cpu) se utiliza para recordar la última dirección en la que la CPU realizó un disparo exitoso. Esta información se almacena para que en el siguiente turno, la CPU pueda realizar disparos adyacentes a la posición anterior, imitando un comportamiento más inteligente.
+
+En la parte del código donde la CPU ha acertado en el último disparo, se guarda la dirección seleccionada en la variable "direccion" en el puntero "*direccion_cpu". Luego, en el siguiente turno, cuando la CPU debe elegir una nueva dirección, se toma en cuenta esta última dirección exitosa. Esto significa que la CPU tratará de disparar en una casilla adyacente a la última casilla exitosa, replicando un comportamiento más estratégico.
+
+Esencialmente, el puntero de dirección ayuda a la CPU a recordar y utilizar información sobre su comportamiento pasado para mejorar su lógica de ataque. Si la CPU ha acertado recientemente, intentará disparar en una dirección cercana a la última posición exitosa.
+
+*/
+
+void ataque_azar(int tablero_victima[][TAB_SIZE], int *acertado, int *acertado_cpu, int casilla_disparada[2], int *direccion_cpu)
+{
+    int x, y;
+    int valido = 0;
+    int direccion; // Variable para almacenar la dirección de ataque
+    int adyacentes = 0;
+
+    // Establecer las coordenadas basadas en la dirección seleccionada
+    x = casilla_disparada[1];
+    y = casilla_disparada[0];
+
+    // Ver si las casillas adyacentes a la casilla anteriormente disparada son válidas
+    if ((x + 1 >= TAB_SIZE || tablero_victima[y][x + 1] == 2 || tablero_victima[y][x + 1] == 3) && (x - 1 < 0 || tablero_victima[y][x - 1] == 2 || tablero_victima[y][x - 1] == 3) && (y + 1 >= TAB_SIZE || tablero_victima[y + 1][x] == 2 || tablero_victima[y + 1][x] == 3) && (y - 1 < 0 || tablero_victima[y - 1][x] == 2 || tablero_victima[y - 1][x] == 3))
+    {
+        adyacentes = 0;
+    }
+    else
+    {
+        adyacentes = 1;
+    }
+
+    if (*acertado_cpu == 1)
+    {
+        do
+        {
+            // Seleccionar una dirección aleatoria: 0 = arriba, 1 = abajo, 2 = izquierda, 3 = derecha
+            direccion = generar_numero(4);
+
+            if (direccion == 0 && y > 0)
+                y--;
+            else if (direccion == 1 && y < TAB_SIZE - 1)
+                y++;
+            else if (direccion == 2 && x > 0)
+                x--;
+            else if (direccion == 3 && x < TAB_SIZE - 1)
+                x++;
+
+            // Verificar que la casilla no haya sido atacada antes
+            if (tablero_victima[y][x] != 2 && tablero_victima[y][x] != 3)
+            {
+                valido = 1;
+            }
+        } while (valido == 0);
+
+        *direccion_cpu = direccion;
+    }
+    else
+    {
+        // Si no acertó en el último disparo, elegir una casilla al azar
+        do
+        {
+            x = generar_numero(TAB_SIZE);
+            y = generar_numero(TAB_SIZE);
+        } while (tablero_victima[y][x] == 2 || tablero_victima[y][x] == 3); // Evitar casillas ya atacadas
+    
+    *direccion_cpu = -1;
+    }
+
+    // Realizar el ataque
+    if (tablero_victima[y][x] == 1)
+    {
+        tablero_victima[y][x] = 3;
+        *acertado = 1;
+        *acertado_cpu = 1;
+        casilla_disparada[0] = y;
+        casilla_disparada[1] = x;
+    }
+    else if (tablero_victima[y][x] == 0)
+    {
+        // En caso de que el disparo pasado haya sido un acierto, se marca la casilla como fallida , sin embargo, no se cambia el valor de acertado_cpu, para que en el siguiente turno se dispare en una casilla adyacente a la original
+        if (*acertado_cpu == 1)
+        {
+            tablero_victima[y][x] = 2;
+            *acertado = 0;
+            *acertado_cpu = 1;
+        }
+        else if (*acertado_cpu == 1 && adyacentes == 0) // En caso de que cpu ya haya intentado con todas las casillas adyacentes, entonces ahora si se cambia el valor de acertado_cpu a 0, para que en el siguiente turno se dispare en una casilla aleatoria
+        {
+            tablero_victima[y][x] = 2;
+            *acertado = 0;
+            *acertado_cpu = 0;
+
+            // Actualizar la última casilla disparada
+            casilla_disparada[0] = y;
+            casilla_disparada[1] = x;
+        }
+    }
+}
