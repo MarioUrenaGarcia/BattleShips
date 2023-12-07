630249bcb2167bb65eda27d57c42d34be9d4c5f5
Ponce <poncerichi4@gmail.com>
2023-12-07 13:24:49 -0600

    implementacion del manual de usuario en la interfaz

--- battleship.h
+++ battleship.h
@@ -47,6 +47,7 @@ int main_menu();                                // Menú principal
 void imprimir_tablero(int tablero[][TAB_SIZE]); // Imprime el tablero de juego, el tablero de entrada depende de la situación.
 void return_to_menu(int *seleccion);            // Regresa al menú principal
 void pantalla_cambio();                         // Pantalla de cambio de jugador
+void manual_usuario();                         // Impresion del manual de usuario
 // Jugabilidad____________________________________________________________________________________
 void partida_cpu();
 void inicializar_tablero(int tablero[][TAB_SIZE]); // Inicializa un tablero de 10x10.
